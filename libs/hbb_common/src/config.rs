6f3d8e8fb253ec3a0406f9d5f73064b50769cc6b
uog001 <77134237+uog001@users.noreply.github.com>
2024-11-22 09:13:34 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["183.237.107.86"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.yatailong.online"];
 pub const PUBLIC_RS_PUB_KEY: &str = "MLk4ZW3W+AEVr8odls8lOAVbhMgu6RYgH++0KTsfUJc=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
